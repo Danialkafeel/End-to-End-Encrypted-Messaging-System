f76a2c884c4bf99db4773b006935ba0f319bec65
Danial Kafeel <danialkafeel@gmail.com>
2021-01-22 14:56:03 +0530

    OOPS client

--- client.py
+++ client.py
@@ -1,64 +1,157 @@
 import threading, socket, os
-import sys
+import sys, hashlib
 load_bal_Addr="127.0.0.1"
 load_bal_port=9999
-isLoggedIn = False
+
+class User(object):
+    def __init__(self):
+        self.username = ""
+        self.isLoggedIn = False
+        self.groupkeys = dict()   # grp name -> keys map
+      #   self.q = FFFFFFFF FFFFFFFF C90FDAA2 2168C234 C4C6628B 80DC1CD1
+      # 29024E08 8A67CC74 020BBEA6 3B139B22 514A0879 8E3404DD
+      # EF9519B3 CD3A431B 302B0A6D F25F1437 4FE1356D 6D51C245
+      # E485B576 625E7EC6 F44C42E9 A637ED6B 0BFF5CB6 F406B7ED
+      # EE386BFB 5A899FA5 AE9F2411 7C4B1FE6 49286651 ECE45B3D
+      # C2007CB8 A163BF05 98DA4836 1C55D39A 69163FA8 FD24CF5F
+      # 83655D23 DCA3AD96 1C62F356 208552BB 9ED52907 7096966D
+      # 670C354E 4ABC9804 F1746C08 CA237327 FFFFFFFF FFFFFFFF
+        self.alpha = 2
+
+    def set_username(self,usern):
+        self.username = usern
+
+    def join_group(self,groupname,key):
+        groupkeys[groupname] = key
+
+    def handle_request(self,connection):        ## DHK recv side
+        data = connection.recv(1024)    ##  Assume this to be public key of sender
+        message = data.decode("utf-8")
+        message = message.split(' ')[1]
+        filepath = '.' + '/' + message
+        if(os.path.isfile(filepath)):
+            message = "Sending file"
+            connection.sendall(message.encode("utf-8"))
+
+            file_ptr = open(filepath, "rb")
+            bytes_read = file_ptr.read(1024)
+            while(bytes_read):
+                connection.send(bytes_read)
+                bytes_read = file_ptr.read(1024)
+        else:
+            message = "File not Present"
+            connection.sendall(message.encode("utf-8"))
+        print("Connection is closed")
+        connection.close()
+        return
+
+    def client_as_server(self,host_addr, port):             ##      Receiver of msg
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((host_addr, int(port)))
+
+        thread_list=[]
+        while True:
+            s.listen()
+            conn, addr = s.accept()
+            thread = threading.Thread(target = handle_request, args= (self,conn, ))
+            thread_list.append(thread)
+            thread.start()
+
+    def client_connection_with_other_client(self,ip,port, msg):     ## Sender of msg
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip,port))
+        ##      Implement DHK sender side here!
+        user = str(rand()%1000)+srt(self.username)
+        private_key = hashlib.sha256(user.encode())
+
+
+
+        s.send(padded_msg.encode('ascii'))
+        data = s.recv(1024).decode("utf-8")
+        print(data)
+
+    def interact_with_server(self):
+        print("Send <Name||Roll no.> <message>")    # 4 ways
+        print("Send_group <No. of groups> <Group no.(s)> <message>")    # 4 ways    send_group 2 g1 g2 this is my g2 dsjfkdlsajfkl
+        print("List")
+        print("Create <Group name>")
+        print("Join <Group name>\n")
+        while True:
+            org_msg=input(":")
+            # padded_msg=appending_dollar(org_msg)+client_IP+"$"+client_PORT
+            tokens=org_msg.split(' ')
+            if (tokens[0].lower()=="send"):     
+                if len(tokens) <  3:
+                    print("Invalid args to <send>")
+                    continue
+                s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((load_bal_Addr,load_bal_port))
+
+                padded_msg = "send$"+tokens[1]          # send$username_of_recv$username_of_sender
+                s.send(padded_msg.encode('utf-8'))
+                data = s.recv(1024).decode("utf-8")     #  1$IP$PORT of receiver.
+                if (data.split('$')[0]=='1'):
+                    recv_ip=data.split('$')[1]
+                    recv_port=data.split('$')[2]
+                    thread = threading.Thread(target = self.client_connection_with_other_client, args= (self,recv_ip,recv_port,tokens[2])) 
+                    thread_list.append(thread)
+                    thread.start()
+                    #---IMP! Yaha check krlo kch sahe krna ho ya add krna ho...not sure about this part
+                else:
+                    print(data.split('$')[1])
+            elif (tokens[0].lower()=="send_group"):     
+                if len(tokens) <  4:
+                    print("Invalid args to <send_group>")
+                    continue
+                s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((load_bal_Addr,load_bal_port))
+
+                padded_msg = "send_group$"+tokens[1]          # send_group$username_of_recv$username_of_sender
+                s.send(padded_msg.encode('utf-8'))
+                data = s.recv(1024).decode("utf-8")     #  1$IP$PORT of receiver.
+                if (data.split('$')[0]=='1'):
+                    recv_ip=data.split('$')[1]
+                    recv_port=data.split('$')[2]
+                    thread = threading.Thread(target = self.client_connection_with_other_client, args= (self,recv_ip,recv_port,tokens[2])) 
+                    thread_list.append(thread)
+                    thread.start()
+                    #---IMP! Yaha check krlo kch sahe krna ho ya add krna ho...not sure about this part
+                else:
+                    print(data.split('$')[1])
+            
+            elif (tokens[0]=="LIST"):
+                s.send(padded_msg.encode('ascii'))
+                data = s.recv(1024).decode("utf-8")
+                if (data.split('$')[0]==1):
+                    for i in data.split('$')[0][1]:         #Assuming ke 2nd token from serve is a list jisme naam honge
+                        print(i)                                    # groups ke
+                else:
+                    print("No Group")
+            
+            elif (tokens[0]=="JOIN"):
+                s.send(padded_msg.encode('ascii'))
+                data = s.recv(1024).decode("utf-8")
+                if (data.split('$')[0]==1):
+                    print("Joined Successfully")        # server returns key of that group
+
+                else:
+                     print("Group not Present")
+            
+            elif (tokens[0]=="CREATE"):
+                s.send(padded_msg.encode('ascii'))
+                data = s.recv(1024).decode("utf-8")
+                if (data.split('$')[0]==1):
+                    print("Created Successfully")
+                else:
+                    print("Group Already Present! Try Again")
+            else:
+                print("Invalid Command")
 
 def appending_dollar(msg):
     s=""
     for i in msg.split(' '):
         s += i.strip()+"$"
     return s
-
-def handle_request(connection):
-    data = connection.recv(1024)
-    message = data.decode("utf-8")
-    message = message.split(' ')[1]
-    filepath = '.' + '/' + message
-    if(os.path.isfile(filepath)):
-        message = "Sending file"
-        connection.sendall(message.encode("utf-8"))
-
-        file_ptr = open(filepath, "rb")
-        bytes_read = file_ptr.read(1024)
-        while(bytes_read):
-            connection.send(bytes_read)
-            bytes_read = file_ptr.read(1024)
-    else:
-        message = "File not Present"
-        connection.sendall(message.encode("utf-8"))
-    print("Connection is closed")
-    connection.close()
-    return
-
-
-
-
-def client_as_server(host_addr, port):
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((host_addr, int(port)))
-
-    thread_list=[]
-    while True:
-        s.listen()
-        conn, addr = s.accept()
-        thread = threading.Thread(target = handle_request, args= (conn, ))
-        thread_list.append(thread)
-        thread.start()
-
-
-
-
-
-def client_connection_with_other_client(ip,port):
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip,port))
-    while true:
-        msg=input()
-        s.send(padded_msg.encode('ascii'))
-        data = s.recv(1024).decode("utf-8")
-        print(data)
-
 
 def main():
     if len(sys.argv) != 3:
@@ -68,21 +161,14 @@ def main():
     client_IP = sys.argv[1]
     client_PORT = sys.argv[2]
     print("Listening on ",client_IP,client_PORT)
+
+    thisUser = User()
     
-    initial_thread = threading.Thread(target = client_as_server, args=(client_IP, client_PORT))
+    initial_thread = threading.Thread(target = thisUser.client_as_server, args=(client_IP, client_PORT))
 
-    #---Connecting to Load Balancer
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((load_bal_Addr,load_bal_port))
-
-    #---Ek Ackn daal sakte hai from Load Balancer after connection with Load Balancer le Welcome ya kch bhe
     print("Available Commands")
     print("Signup <Name> <Roll no.> <Password>")
-    print("Login <Name||Roll no.> <Password>")
-    print("Send <Name||Roll no.> <message>")
-    print("List")
-    print("Create <Group name>")
-    print("Join <Group name>\n")
+    print("Login <Name||Roll no.> <Password>\n")
     while True:
         org_msg=input(":")
         # padded_msg=appending_dollar(org_msg)+client_IP+"$"+client_PORT
@@ -92,67 +178,41 @@ def main():
             if len(tokens) != 4:
                 print("Invalid args to <Signup>")
                 continue
+            #---Connecting to Load Balancer
+            s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((load_bal_Addr,load_bal_port))
+            # print(s.getsockname()[1])
             username = tokens[1]+tokens[2]
             padded_msg = "signup$"+username+"$"+tokens[3]
-            s.send(padded_msg.encode('ascii'))
-            data = s.recv(1024).decode("ascii") 
+            s.send(padded_msg.encode('utf-8'))
+            data = s.recv(1024).decode("utf-8") 
             if (data.split('$')[0]==1):
-                print("Sign up Successful! ")
+                print("Sign up Successful!")
             else:
                 print("Sign up Failed! Try Again")
+            s.close()
         
         elif (tokens[0].lower()=="login"):
             if len(tokens) != 3:
                 print("Invalid args to <Login>")
                 continue            
+            s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print(s.getsockname()[1])
+            s.connect((load_bal_Addr,load_bal_port))
             padded_msg = "login$"+tokens[1]+"$"+tokens[2]
-            s.send(padded_msg.encode('ascii'))
+            s.send(padded_msg.encode('utf-8'))
             data = s.recv(1024).decode("utf-8") 
-            if (data.split('$')[0]==1):
-                print("Login in Successful! ")
+            if (data.split('$')[0]=='1'):
+                print("Login in Successful!")
+                s.close()
+                thisUser.set_username(token[1])
+                ####                Add grps name+keys on login
+                thisUser.interact_with_server()
+                break
             else:
-                print("Wrong Credentianals or Sign Up First")
-
-        elif (tokens[0].lower()=="send"):
-            s.send(padded_msg.encode('ascii'))
-            data = s.recv(1024).decode("utf-8")
-            if (data.split('$')[0]==1):
-                recv_ip=data.split('$')[-2]
-                recv_port=data.split('$')[-1]
-                thread = threading.Thread(target = client_connection_with_other_client, args= (recv_ip,recv_port)) 
-                thread_list.append(thread)
-                thread.start()
-                #---IMP! Yaha check krlo kch sahe krna ho ya add krna ho...not sure about this part
-            else:
-                print("USERNAME Not Present or Login or Sign up First")
-        
-        elif (tokens[0]=="LIST"):
-            s.send(padded_msg.encode('ascii'))
-            data = s.recv(1024).decode("utf-8")
-            if (data.split('$')[0]==1):
-                for i in data.split('$')[0][1]:         #Assuming ke 2nd token from serve is a list jisme naam honge
-                    print(i)                                    # groups ke
-            else:
-                print("No Group")
-        
-        elif (tokens[0]=="JOIN"):
-            s.send(padded_msg.encode('ascii'))
-            data = s.recv(1024).decode("utf-8")
-            if (data.split('$')[0]==1):
-                print("Joined Successfully")
-            else:
-                 print("Group not Present")
-        
-        elif (tokens[0]=="CREATE"):
-            s.send(padded_msg.encode('ascii'))
-            data = s.recv(1024).decode("utf-8")
-            if (data.split('$')[0]==1):
-                print("Created Successfully")
-            else:
-                print("Group Already Present! Try Again")
-        
-        padded_msg=""
-        #Session End ka bhe kch msg daal sakte hai quit ya kch bhe
+                print(data.split('$')[1])
+        else:
+            print("Please signup/login first!")
 
 if __name__ == '__main__':
     main()
